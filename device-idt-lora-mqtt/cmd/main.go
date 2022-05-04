8627ea3242934f8acde392629035c96f6282a516
alanleetibco <53249447+alanleetibco@users.noreply.github.com>
2022-05-04 16:01:22 +1000

    new idt device

---
+++ device-idt-lora-mqtt/cmd/main.go
@@ -0,0 +1,22 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2018 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	device_generic_mqtt "github.com/TIBCOSoftware/labs-air/edgexfoundry/device-idt-lora-mqtt"
+	"github.com/TIBCOSoftware/labs-air/edgexfoundry/device-idt-lora-mqtt/internal/driver"
+	"github.com/edgexfoundry/device-sdk-go/v2/pkg/startup"
+)
+
+const (
+	serviceName string = "device-idt-lora-mqtt"
+)
+
+func main() {
+	sd := driver.NewProtocolDriver()
+	startup.Bootstrap(serviceName, device_generic_mqtt.Version, sd)
+}
